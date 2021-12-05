8819ff3db067288c2383bacb4cadace22409ee19
nguyenbaolong11062000 <52575643+nguyenbaolong11062000@users.noreply.github.com>
2021-12-05 12:51:51 +0700

    update admin page

--- nha_sach/__init__.py
+++ nha_sach/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_admin import Admin
 from flask_login import LoginManager
+from flask_babelex import Babel #bộ dịch hổ trợ sẵn trong phân hệ admin
 
 app = Flask(__name__)
 app.secret_key = '3^@&@&!&(@*UGUIEIU&@^!*(@&*(SS'
@@ -10,5 +11,11 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
 db = SQLAlchemy(app=app)
 admin = Admin(app=app, name='Nhà Sách LT',
-              template_mode="bootstrap3")
+              template_mode="bootstrap4")
 login = LoginManager(app=app)
+babel = Babel(app=app)
+
+@babel.localeselector
+def get_locale():
+        # Phương thức chuyển đổi thành tiếng việt trên các chức năng hoặc tiêu đề trên trang admin('địa phương hóa')
+        return 'vi'
--- nha_sach/admin.py
+++ nha_sach/admin.py
@@ -18,45 +18,118 @@ class GioiThieu(BaseView):
         return self.render('admin/about-us.html')
 
 class BangLoaiSach(ModelView):
-    column_display_pk = True;
+    column_display_pk = True
     can_export = True
+    can_view_details = True
+    details_modal = True
+    edit_modal = True #_modal là hiển thị ra box modal chứ không phải chuyển qua trang mới
+    create_modal = True
+    column_filters = ['MieuTaLoaiSach'] #lọc ra các kết quả với biến trong ['?'] là giá trị cột bên model
+    column_searchable_list = ['MieuTaLoaiSach'] #tiềm kiếm ra các kết quả mong muốn với biến trong ['?'] là giá trị cột bên model
+    column_labels = {
+        'MaLoaiSach': 'Mã Loại Sách',
+        'MieuTaLoaiSach': 'Tên Loại Sách'
+    } #sửa tên các cột trên trang chủ admin theo ý mình với giá trị key '?' là giá trị cột bên model
+    form_excluded_columns = ['sach']
 
     def is_accessible(self):
         return current_user.is_authenticated
 
 
 class BangSach(ModelView):
-    column_display_pk = True;
+    column_display_pk = True
     can_export = True
+    can_view_details = True
+    details_modal = True
+    edit_modal = True
+    create_modal = True
+    column_filters = ['TuaSach', 'MoTaSach', 'GiaBia']
+    column_searchable_list = ['TuaSach', 'MoTaSach', 'GiaBia']
+    column_labels = {
+        'MaSach': 'Mã Sách',
+        'TuaSach': 'Tên Sách',
+        'MoTaSach': 'Mô Tả Sách',
+        'GiaBia': 'Giá Bìa',
+        'HinhAnh': 'Hình Ảnh',
+        'NamXuatBan': 'Năm Xuất Bản',
+        'LoaiSach': 'Loại Sách'
+    }
+    form_excluded_columns = ['chiTiet_DH', 'tg_vietsach'] #danh sách các cột không hiển thị trong form nhập mới một sản phẩm hoặc sửa thông tin sản phẩm
 
     def is_accessible(self):
         return current_user.is_authenticated
 
 class BangTacGia(ModelView):
-    column_display_pk = True;
+    column_display_pk = True
     can_export = True
+    can_view_details = True
+    details_modal = True
+    edit_modal = True
+    create_modal = True
+    column_filters = ['Ho', 'Ten']
+    column_searchable_list = ['Ho', 'Ten']
+    column_labels = {
+        'MaTG': 'Mã Tác Giả',
+        'Ho': 'Họ',
+        'Ten': 'Tên',
+        'HinhAnh': 'Hình Ảnh',
+        'TieuSu': 'Tiểu Sử'
+    }
+    form_excluded_columns = ['tg_vietsach']
 
     def is_accessible(self):
         return current_user.is_authenticated
 
 class BangTacGiaVietSach(ModelView):
-    column_display_pk = True;
+    column_display_pk = True
     can_export = True
+    can_view_details = True
+    details_modal = True
+    edit_modal = True
+    create_modal = True
+    column_labels = {
+        'TacGia': 'Tác Giả',
+        'Sach': 'Sách'
+    }
 
     def is_accessible(self):
         return current_user.is_authenticated
 
 
 class BangDonHang(ModelView):
-    column_display_pk = True;
+    column_display_pk = True
     can_export = True
+    can_view_details = True
+    details_modal = True
+    edit_modal = True
+    create_modal = True
+    column_filters = ['NgayMua']
+    column_labels = {
+        'MaDH': 'Mã Đơn Hàng',
+        'NgayMua': 'Ngày Mua Hàng',
+        'TenKH': 'Khách Hàng'
+    }
+    form_excluded_columns = ['chiTiet_DH']
 
     def is_accessible(self):
         return current_user.is_authenticated
 
 class BangChiTietDonHang(ModelView):
-    column_display_pk = True;
+    column_display_pk = True
     can_export = True
+    can_view_details = True
+    details_modal = True
+    edit_modal = True
+    create_modal = True
+    column_filters = ['SoLuong', 'GiaBan', 'TyLeGiamGia']
+    column_searchable_list = ['SoLuong', 'GiaBan', 'TyLeGiamGia']
+    column_labels = {
+        'DonHang': 'Đơn Hàng',
+        'Sach': 'Sách',
+        'SoLuong': 'Số Lượng',
+        'GiaBan': 'Giá Bán',
+        'TyLeGiamGia': 'Tỷ Lệ Giảm Giá'
+    }
 
     def is_accessible(self):
         return current_user.is_authenticated
@@ -64,8 +137,28 @@ class BangChiTietDonHang(ModelView):
 
 
 class BangKhacHang(ModelView):
-    column_display_pk = True;
+    column_display_pk = True
     can_export = True
+    can_view_details = True
+    details_modal = True
+    edit_modal = True
+    create_modal = True
+    column_filters = ['Ho', 'Ten', 'GioiTinh']
+    column_searchable_list = ['Ho', 'Ten', 'GioiTinh']
+    column_labels = {
+        'MaKH': 'Mã Khách Hàng',
+        'Ho': 'Họ',
+        'Ten': 'Tên',
+        'Email': 'Email',
+        'TenDangNhap': 'Tên Đăng Nhập',
+        'MatKhau': 'Mật Khẩu',
+        'GioiTinh': 'Giới Tính',
+        'HinhAnh': 'Hình Ảnh',
+        'SoDT': 'Số Điện Thoại',
+        'HoatDonng': 'Còn Sử Dụng',
+        'VaiTro': 'Phân Quyền',
+    }
+    form_excluded_columns = ['don_hang']
 
     def is_accessible(self):
         return current_user.is_authenticated
